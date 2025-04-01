ff27d62153847bdb0982aeab257a58682004a5ef
miladnader19933 <miladnader19933@gmail.com>
2025-04-01 09:00:51 +0300

    8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ContextProvider } from "./context/Context.jsx";
 import App from "./App.jsx";
 import { HelmetProvider } from "react-helmet-async";
